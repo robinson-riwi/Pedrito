1f6b7e418b5660559da63dfafe48f446cf3491cd
Robinson Andres Cortes <147889972+robinson-riwi@users.noreply.github.com>
2025-09-29 18:12:32 -0500

    nicee

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Pedrito.Data;
+using Pedrito.Models;
 
 namespace Pedrito.Controllers;
 
@@ -28,5 +29,33 @@ public class UsersController : Controller
         }
 
         return View(user);
+    }
+
+    public IActionResult Store([Bind("Names,LastNames,Email")] User user)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(user);
+            _context.SaveChanges();
+            TempData["message"] = "Niceee usuario creado :)";
+            return RedirectToAction(nameof(Index));
+        }
+
+        return BadRequest();
+    }
+
+    [HttpDelete]
+    public IActionResult Destroy(int id)
+    {
+        var user = _context.users.Find(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        _context.users.Remove(user);
+        _context.SaveChanges();
+        TempData["message"] = "El usuario ha eliminado";
+        return RedirectToAction(nameof(Index));
     }
 }
